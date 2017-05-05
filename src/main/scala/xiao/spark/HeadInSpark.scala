6d18b6a3ef3b18b3687c0dc3f19a59c2a1c0af87
BaoQiang <mailbaoqiang@gmail.com>
2017-05-05 19:05:16 +0800

    init spark project

---
+++ src/main/scala/xiao/spark/HeadInSpark.scala
@@ -0,0 +1,20 @@
+package xiao.spark
+
+/**
+  * head in spark
+  *
+  * @author BaoQiang
+  * @version 2.0
+  * @date: 2017/5/5 18:58
+  */
+
+object HeadInSpark {
+
+  def run(): Unit = {
+    print("333")
+  }
+
+  def main(args: Array[String]): Unit = {
+    run()
+  }
+}
